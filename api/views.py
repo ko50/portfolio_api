c92bb9b053728f419aaf97a532c10d85f5280916
ko5 <kotyana1331@gmail.com>
2021-04-11 00:09:49 +0900

    [追加] viewsetを追加

--- api/views.py
+++ api/views.py
@@ -1,2 +1,48 @@
-from django.shortcuts import render
+from rest_framework import viewsets, generics
 
+from .models import *
+from .serializer import *
+
+# Normal
+
+
+class AboutMeViewSet(viewsets.ModelViewSet):
+    queryset = AboutMeInformation.objects.all()
+    serializer_class = AboutMeSerializer
+
+
+class SkillViewSet(viewsets.ModelViewSet):
+    queryset = Skill.objects.all()
+    serializer_class = SkillSerializer
+
+
+class WorkViewSet(viewsets.ModelViewSet):
+    queryset = Work.objects.all()
+    serializer_class = WorkSerializer
+
+
+class ContactViewSet(viewsets.ModelViewSet):
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
+
+# ListAPIView
+
+
+class AboutMeListAPIView(generics.ListAPIView):
+    queryset = AboutMeInformation.objects.all()
+    serializer_class = AboutMeSerializer
+
+
+class SkillListAPIView(generics.ListAPIView):
+    queryset = Skill.objects.all()
+    serializer_class = SkillSerializer
+
+
+class WorkListAPIView(generics.ListAPIView):
+    queryset = Work.objects.all()
+    serializer_class = WorkSerializer
+
+
+class ContactListAPIView(generics.ListAPIView):
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
